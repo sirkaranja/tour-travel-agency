92c8ecabd264904e600f3c4174f50a47b191c1f4
+ <+>
2023-06-01 00:53:45 +0300

    navbar component updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './navbar.css'
+import {SiYourtraveldottv} from 'react-icons/si'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <section className='navBarSection'>
+    <div className="header">
+      <div className="logoDiv">
+        <a href="#" className="logo">
+          <h1><SiYourtraveldottv className="icons" /></h1>
+        </a>
+      </div>
+    </div>
+   </section>
   )
 }
 
