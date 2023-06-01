a47294bd154422fe59070f50f3dd9b3633c4428b
+ <+>
2023-06-01 12:03:36 +0300

    done with home section

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from 'react'
 import './app.css'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
-import Popular from './Components/Popular/Popular'
-import Offers from './Components/Offers/Offers'
-import About from './Components/About/About'
-import Blog from './Components/Blog/Blog'
-import Footer from './Components/Footer/Footer'
+// import Popular from './Components/Popular/Popular'
+// import Offers from './Components/Offers/Offers'
+// import About from './Components/About/About'
+// import Blog from './Components/Blog/Blog'
+// import Footer from './Components/Footer/Footer'
 
 export const App = () => {
   return (
     <>
     <Navbar />
-    {/* <Home />
-    <Popular />
-    <Offers />
-    <About />
-    <Blog />
-    <Footer /> */}
+    <Home />
+   
     </>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,42 @@ import './home.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="secContainer container">
+        <div className="homeText">
+          <h1 className="title">
+            Plan Your Travel With Tripelo
+          </h1>
+
+
+          <p className="subTitle">
+            Travel to your favourite city with respectful of the environment
+          </p>
+
+          <button className='btn'>
+            <a href="#">Explore Now</a>
+
+          </button>
+        </div>
+        <div className="homeCard grid">
+          <div className="locationDiv">
+            <label htmlFor="location">Location</label>
+            <input type="text" placeholder='Dream Destination'  />
+          </div>
+          <div className="distDiv">
+            <label htmlFor="distance">Distance</label>
+            <input type="text" placeholder='11 metres'  />
+          </div>
+          <div className="priceDiv">
+            <label htmlFor="location">price</label>
+            <input type="text" placeholder='$140-$500'  />
+          </div>
+          <button className='btn '>Search</button>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
