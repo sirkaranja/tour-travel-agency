040b0ba70189b4b0c3810923d0a72a1df5ee39ed
+ <sirkaranja12@gmail.com>
2023-06-18 00:22:22 +0300

    about component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Popular from './Components/Popular/Popular'
 import Offers from './Components/Offers/Offers'
-// import About from './Components/About/About'
+import About from './Components/About/About'
 // import Blog from './Components/Blog/Blog'
 // import Footer from './Components/Footer/Footer'
 
@@ -15,6 +15,7 @@ export const App = () => {
     <Home />
     <Popular />
     <Offers />
+    < About />
    
     </>
   )
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './about.css'
 
-export const About = () => {
+//images
+import img from '../../Assets/hiking.png'
+import img1 from '../../Assets/climbing.jpg'
+import img2 from '../../Assets/mountain.jpg'
+
+//video
+import video from '../../Assets/hike.mp4'
+
+
+ const About = () => {
   return (
-    <div>About</div>
+    <section className='about section'>
+      <div className="secContainer">
+        <h2 className="title">
+          Why Hiking?
+          </h2>
+          <div className="mainContainer container grid">
+            <div className="singleItem">
+              <img src={img} alt="Image Name" />
+              <h3>100+ Mountains</h3>
+              <p>
+                Research shows that a chance to break away from the noraml rhythms of
+                daily life reduces stress and improves health and well-being.
+              </p>
+            </div>
+            <div className="singleItem">
+              <img src={img1} alt="Image Name" />
+              <h3>1000+ Hiking</h3>
+              <p>
+                Research shows that a chance to break away from the noraml rhythms of
+                daily life reduces stress and improves health and well-being.
+              </p>
+            </div>
+            <div className="singleItem">
+              <img src={img2} alt="Image Name" />
+              <h3>2000+ Customers</h3>
+              <p>
+                Research shows that a chance to break away from the noraml rhythms of
+                daily life reduces stress and improves health and well-being.
+              </p>
+            </div>
+          </div>
+          <div className="videoCard container">
+            <div className="cardContent grid">
+              <div className="cardText">
+                <h2>Wonderful House Experience in there!</h2>
+                <p>
+                  The adventure subranking is based on an 
+                  equally weighted average of scores from
+                  five country
+                </p>
+              </div>
+              <div className="cardVideo">
+                <video src={video} autoPlay loop muted type="video/mp4">
+
+                </video>
+              </div>
+
+              
+            </div>
+          </div>
+          </div>
+    </section>
   )
 }
 
